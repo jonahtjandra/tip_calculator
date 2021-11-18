3e23bfbed4930f4d65b82f55f577d30bebf3377a
Jonah Tjandra <jonahtjandra@wirelessprv-10-194-33-27.near.illinois.edu>
2021-11-17 21:04:47 -0600

    added dark mode and extra functionalities

--- tip_calculator/SettingsViewController.swift
+++ tip_calculator/SettingsViewController.swift
@@ -7,6 +7,18 @@
 
 import UIKit
 
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
 class SettingsViewController: UIViewController {
     
     @IBOutlet weak var tip1Edit: UITextField!
@@ -15,12 +27,59 @@ class SettingsViewController: UIViewController {
     
     @IBOutlet weak var tip3Edit: UITextField!
     
+    @IBOutlet weak var titleSetting: UILabel!
+    
+    @IBOutlet weak var tip1Label: UILabel!
+    
+    @IBOutlet weak var tip2Label: UILabel!
+    
+    @IBOutlet weak var tip3Label: UILabel!
+    
+    @IBOutlet weak var darkModeLabel: UILabel!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    
+    @IBOutlet weak var darkModeButton: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        tip1Edit.keyboardType = UIKeyboardType.decimalPad;
+        tip2Edit.keyboardType = UIKeyboardType.decimalPad;
+        tip3Edit.keyboardType = UIKeyboardType.decimalPad;
+        self.hideKeyboardWhenTappedAround();
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        tip1Edit.becomeFirstResponder();
+        if (UserDefaults.standard.integer(forKey: "dark") == 1) {
+            darkModeButton.setOn(true, animated: false)
+            setThemeLight();
+        } else {
+            darkModeButton.setOn(false, animated: false)
+            setThemeDark();
+        }
+    }
+    
+    func setThemeDark() {
+        self.view.backgroundColor = UIColor.white;
+        titleSetting.textColor = UIColor.black;
+        tip1Label.textColor = UIColor.black;
+        tip2Label.textColor = UIColor.black;
+        tip3Label.textColor = UIColor.black;
+        darkModeLabel.textColor = UIColor.black;
+        saveButton.tintColor = UIColor.black;
+    }
+    
+    func setThemeLight() {
+        self.view.backgroundColor = UIColor(red:0.15, green:0.15, blue:0.15, alpha:1);
+        titleSetting.textColor = UIColor.white;
+        tip1Label.textColor = UIColor.white;
+        tip2Label.textColor = UIColor.white;
+        tip3Label.textColor = UIColor.white;
+        darkModeLabel.textColor = UIColor.white;
+        saveButton.tintColor = UIColor.white;
+    }
     
     @IBAction func saveClicked(_ sender: Any) {
         let defaults = UserDefaults.standard;
@@ -33,7 +92,18 @@ class SettingsViewController: UIViewController {
         tip3Edit.text = "";
     }
     
-
+    @IBAction func toggleDarkMode(_ sender: UISwitch) {
+        let defaults = UserDefaults.standard;
+        if (sender.isOn == true) {
+            defaults.set(1, forKey: "dark");
+            setThemeLight();
+        } else {
+            defaults.set(0, forKey: "dark");
+            setThemeDark();
+        }
+        
+    }
+    
     /*
     // MARK: - Navigation
 
--- tip_calculator/ViewController.swift
+++ tip_calculator/ViewController.swift
@@ -15,31 +15,75 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tipControl: UISegmentedControl!
     
-    @IBOutlet weak var totalLabel: UILabel!
-    
     @IBOutlet weak var partySizeTextField: UITextField!
     
     @IBOutlet weak var perPersonTotal: UILabel!
     
+    @IBOutlet weak var billAmountLabel: UILabel!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabelText: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var partySizeLabel: UILabel!
+    
+    @IBOutlet weak var perPersonLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        billAmountTextField.keyboardType = UIKeyboardType.decimalPad;
+        partySizeTextField.keyboardType = UIKeyboardType.decimalPad;
+        self.hideKeyboardWhenTappedAround();
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+        billAmountTextField.becomeFirstResponder();
         partySizeTextField.text = "1";
         // Do any additional setup after loading the view.
         let defaults = UserDefaults.standard;
         tipControl.setTitle(defaults.string(forKey: "tip1") ?? "15%", forSegmentAt: 0);
         tipControl.setTitle(defaults.string(forKey: "tip2") ?? "18%", forSegmentAt: 1);
         tipControl.setTitle(defaults.string(forKey: "tip3") ?? "20%", forSegmentAt: 2);
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        print("view will appear")
         // This is a good place to retrieve the default tip percentage from UserDefaults
         // and use it to update the tip amount
-        let defaults = UserDefaults.standard;
         tipControl.setTitle((defaults.string(forKey: "tip1") ?? "15")+"%", forSegmentAt: 0);
         tipControl.setTitle((defaults.string(forKey: "tip2") ?? "18")+"%", forSegmentAt: 1);
         tipControl.setTitle((defaults.string(forKey: "tip3") ?? "20")+"%", forSegmentAt: 2);
+        if (defaults.integer(forKey: "dark") == 1) {
+            setThemeDark();
+        } else {
+            setThemeLight();
+        }
+    }
+    
+    func setThemeDark() {
+        self.view.backgroundColor = UIColor(red:0.15, green:0.15, blue:0.15, alpha:1);
+        tipLabel.textColor = UIColor.white;
+        tipAmountLabel.textColor = UIColor.white;
+        billAmountLabel.textColor = UIColor.white;
+        totalLabel.textColor = UIColor.white;
+        totalLabelText.textColor = UIColor.white;
+        partySizeLabel.textColor = UIColor.white;
+        perPersonLabel.textColor = UIColor.white;
+        perPersonTotal.textColor = UIColor.white;
+    }
+    
+    func setThemeLight() {
+        self.view.backgroundColor = UIColor.white;
+        tipLabel.textColor = UIColor.black;
+        tipAmountLabel.textColor = UIColor.black;
+        billAmountLabel.textColor = UIColor.black;
+        totalLabel.textColor = UIColor.black;
+        totalLabelText.textColor = UIColor.black;
+        partySizeLabel.textColor = UIColor.black;
+        perPersonLabel.textColor = UIColor.black;
+        perPersonTotal.textColor = UIColor.black;
     }
     
     func calculateTipHelper() {
